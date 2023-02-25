dbaedd48f13ce709fd7115be83acc4cb44c478b3
Vishnuprasad97 <vishnuprasad9391@gmail.com>
2023-02-25 12:55:36 +0530

    added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestcomponentComponent } from './testcomponent/testcomponent.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestcomponentComponent
+    TestcomponentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
